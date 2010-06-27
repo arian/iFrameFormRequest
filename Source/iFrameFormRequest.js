b9e4ed329285a0af8838845071324249289605d4
Arian <arian@aryweb.nl>
2010-06-28 01:06:44 +0200

    Use the Events mixin

--- Source/iFrameFormRequest.js
+++ Source/iFrameFormRequest.js
@@ -26,7 +26,7 @@ provides: [Element.iFrameFormRequest, iFrameFormRequest]
 
 var iFrameFormRequest = new Class({
 	
-	Implements: Options,
+	Implements: [Options, Events],
 	
 	options: { /*
 		onRequest: $empty,
@@ -34,17 +34,17 @@ var iFrameFormRequest = new Class({
 		onFailure: $empty */
 	},
 	
-	initialize: function(formElmt,options){
+	initialize: function(formElmt, options){
 		this.setOptions(options);
 		this.frameId ='f' + Math.floor(Math.random() * 99999);
 		this.loading = false;
 
 		this.formElmt = document.id(formElmt)
-			.set('target',this.frameId)
-			.addEvent('submit',function(){
+			.set('target', this.frameId)
+			.addEvent('submit', function(){
 				this.loading = true;
-				this.options.onRequest();
-			}.bind(this));	
+				this.fireEvent('request');
+			}.bind(this));
 
 		this.iframe = new IFrame({
 			name: this.frameId,
@@ -53,18 +53,18 @@ var iFrameFormRequest = new Class({
 			},
 			src: 'about:blank',
 			events: {
-				load: function(self){
+				load: function(){
 					if (self.loading) {				
 						var doc = document.getElementById(self.frameId).contentWindow.document;
 						if (doc) {
 							if (doc.location.href == 'about:blank') {
-								self.options.onFailure();
+								self.fireEvent('failure');
 							}
 							if ($type(self.options.onComplete) == 'function') {
-								self.options.onComplete(doc.body.innerHTML);
+								self.fireEvent('complete', doc.body.innerHTML);
 							}
 						} else {
-							self.options.onFailure();
+							self.fireEvent('failure');
 						}
 						self.loading = false;
 					}

dd69430430d4da8a1e3ae09c2e86525b94f78d93
Georg Nagel <georg.nagel@stereomorph.net>
2011-08-06 15:29:34 +0200

    always calling this.formEvent() on manual submit()

--- Source/iFrameFormRequest.js
+++ Source/iFrameFormRequest.js
@@ -72,7 +72,7 @@ var iFrameFormRequest = new Class({
 
 	send: function(){
 		this.form.submit();
-		if (this.options.eventName == 'submit') this.formEvent();
+		this.formEvent();
 	},
 
 	attach: function(){
